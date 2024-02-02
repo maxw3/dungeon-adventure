e2691a794862244a98115ce20cc1d0dfa681aff7
Max <maxnazarchuk@gmail.com>
2024-02-02 15:38:40 -0600

    Added a temp room and floor classes

--- src/Room.java
+++ src/Room.java
@@ -3,40 +3,46 @@ public class Room {
     private Item[] myItems;
     private DungeonCharacter[] myDungeonCharacters;
     
-    private boolean myNorth;
-    private boolean myEast;
-    private boolean mySouth;
-    private boolean myWest;
     private int myEvent;
     private boolean myExplored;
 
+    private char[][] myRoomChars; 
+
     Room(){
-        
+        myItems = new Item[0];
+        myDungeonCharacters = new DungeonCharacter[0];
+
+        myRoomChars = new char[3][3];
+
+        myRoomChars[0][0] = '*';
+        myRoomChars[0][1] = '-';
+        myRoomChars[0][2] = '*';
+
+        myRoomChars[1][0] = '|';
+        myRoomChars[1][1] = ' ';
+        myRoomChars[1][2] = '|';
+
+        myRoomChars[2][0] = '*';
+        myRoomChars[2][1] = '-';
+        myRoomChars[2][2] = '*';
+    }
+
+    public char[] getRow(int theRow){
+        return myRoomChars[theRow];
     }
 
     @Override
     public String toString(){
         StringBuilder sb = new StringBuilder();
 
-        // Print First Row
-        sb.append('*');
-        if(myNorth){
-            sb.append("-\n");
-        }else{
-            sb.append("*\n");
+        for(char[] a: myRoomChars){
+            for(char c: a){
+                sb.append(c);
+            }
+            sb.append("\n");
         }
 
-        if(myEast){
-            sb.append("|");
-        }else{
-            sb.append("*");
-        }
-
-        if(myItems.length > 1){
-            sb.append('m');
-        }
-
-        return "4";
+        return sb.toString();
     }
 
 }
