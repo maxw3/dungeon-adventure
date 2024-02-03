982a2f068aee4661bd38ffa85de00555446743a7
Max <maxnazarchuk@gmail.com>
2024-02-02 23:00:51 -0600

    Changed the floor generation

--- src/App.java
+++ src/App.java
@@ -3,10 +3,6 @@ public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Welcome to the Dungeon Adventure!");
 
-        Room r = new Room();
-
-        System.out.println(r.toString());
-
         Floor f = new Floor();
 
         System.out.println(f.toString());
