5b114a9ee2c73e5f55b6d52699a5d26d566dc95b
Max <maxnazarchuk@gmail.com>
2024-02-05 00:16:38 -0500

    Added a floor size constructor and re-typed the floor size

--- src/App.java
+++ src/App.java
@@ -2,7 +2,7 @@ public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Welcome to the Dungeon Adventure!");
 
-        Floor f = new Floor();
+        Floor f = new Floor(6);
 
         System.out.println(f.toString());
     }
